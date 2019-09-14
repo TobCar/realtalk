3045fc61c9d0dae4c9fdc77a8db97828f45f4ab7
shannonjanehogan <shannon.jane.hogan@gmail.com>
2019-09-14 01:53:59 -0400

    Add video model

--- server/app/api_v1/routes.py
+++ server/app/api_v1/routes.py
@@ -13,7 +13,15 @@ class Status(Resource):
 class Video(Resource):
     def post(self):
         json_data = request.get_json(force=True)
-        video_id = json_data['video_id']
+        video_id = json_data['id']
+        url = json_data['url']
+        tag = json_data['tag']
+        ## TODO Call Youtube API with video id
+        ## TODO transform data into flac.file
+        ## TODO call model script
+        ## TODO  grab return value of model endpoint
+        ## TODO save return value to database
+        ## TODO return value to client
         print("video_id is: ", video_id)
 
         return {
