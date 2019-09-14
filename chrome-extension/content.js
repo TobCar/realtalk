ac2eddb2df1654f53865d52302a1b3d6315b61b4
shannonjanehogan <shannon.jane.hogan@gmail.com>
2019-09-14 16:26:28 -0400

    Fix error so that 0 and 1 are both checked in if statement

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -8,16 +8,17 @@ var videoId = url.toString().substring(32, url.toString().length);
  *  MAKE REQUEST AND GET ARRAY
  */
 
-var res = [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0,0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1];
+var res = [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0,0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1];
 
 var timer = setInterval(function() {
     var yt = document.querySelector('.video-stream');
     var time = Math.floor(yt.currentTime);
-    if (res[time]) {
+    if (res[time] !== undefined) {
         if (res[time] === 0) {
-            chrome.runtime.sendMessage({ message: "Not a Voice" });
+            chrome.runtime.sendMessage({ message: "fake" });
         } else if (res[time] === 1) {
-            chrome.runtime.sendMessage({ message: "Real Voice" });
+            chrome.runtime.sendMessage({ message: "real" });
         }
     }
-}, 1000);
+}, 5000);
+// TODO double check that 5000 interval distance is correct
