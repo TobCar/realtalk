59c02884000d98ae0e4a9d01fe0d6eda273bb64f
Kyle Meade <kyle.meade37@gmail.com>
2019-09-14 13:24:14 -0400

    some more stuff

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,25 +1,41 @@
 'use strict';
 
-/**
- *  For anyone reading this code,
- * I used this way instead of web navigation cause since youtube is an app,
- * URL changes aren't making actual page requests, and is just referenced
- * by the app's router for the components, so webNavigation events don't work.
- * Had to "hack" around to make an event for watching youtube videos.
- */
-
 // Setup event listener for when user goes on a youtube video and gets the ID
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         var url = tabs[0].url;
         if (url.toString().substring(0, 29) === 'https://www.youtube.com/watch') {
-            chrome.notifications.create({
-                type: 'basic',
-                iconUrl: 'logo.png',
-                title: 'Watching video',
-                message: `ID: ${url.toString().substring(32, url.toString().length)}`,
-                priority: 0
+            chrome.tabs.executeScript({
+                file: "content.js"
             });
+        } else {
+            chrome.browserAction.setPopup({ popup: "popup.html" });
         }
     });
+});
+
+// Expect messages from content scripts to change the popup
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.loading && (request.loading == "true")) {
+        chrome.browserAction.setPopup({ popup: "popup-loading.html" });
+    }
+    if (request.good) {
+        if (request.good == "true") {
+            chrome.browserAction.setPopup({ popup: "popup-real.html" });
+        }
+    }
+    var msg = request.message;
+    if (msg) {
+        if (msg == "loading") {
+            chrome.browserAction.setPopup({ popup: "popup-loading.html" });
+        } else if (msg == "real") {
+            chrome.browserAction.setPopup({ popup: "popup-real.html" });
+        } else if (msg == "fake") {
+            chrome.browserAction.setPopup({ popup: "popup-fake.html" });
+        } else if (msg == "default") {
+            chrome.browserAction.setPopup({ popup: "popup.html" });
+        } else {
+            chrome.browserAction.setPopup({ popup: "popup.html" });
+        }
+    }
 });
--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,1 +1,26 @@
-alert('nice');
+'use strict';
+
+
+var url = location.href;
+
+var videoId = url.toString().substring(32, url.toString().length);
+
+/**
+ *  MAKE REQUEST AND GET ARRAY
+ */
+var res = [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0,0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1];
+
+var yt = document.querySelector('.video-stream');
+var timer;
+
+timer = setInterval(function() {  
+    yt = document.querySelector('.video-stream');
+    var time = Math.floor(yt.getCurrentTime());
+    if (res[time]) {
+        if (res[time] === 0) {
+            chrome.runtime.sendMessage({ message: "fake" });
+        } else if (res[time] === 1) {
+            chrome.runtime.sendMessage({ message: "real" });
+        }
+    }
+}, 1000);
