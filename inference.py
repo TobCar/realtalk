cae6fd8cdcc415f6050e57d881956e9e60c63292
TobCar <TobCar@users.noreply.github.com>
2019-09-14 12:49:37 -0400

    Fix inference

--- inference.py
+++ inference.py
@@ -1,6 +1,6 @@
 from data_wrangling import get_windows_from_array
 from inference_helpers import inference_setup, round_predictions
-from librosa.core import load
+from pydub import AudioSegment
 import numpy as np
 
 
@@ -9,9 +9,18 @@ model = inference_setup(Hz)
 
 
 def classify(audio_file_path):
-    audio_arr = load(audio_file_path, sr=Hz)
+    """
+    :param audio_file_path: Path to an mp3 file.
+    :return: Array of 1 second values. Each entry in the array is whether that second is a real voice or not.
+    """
+    # We assume the mp3 file has 16 bits per sample so there are 16,000 samples in a second.
+    audio_arr = np.array(AudioSegment.from_mp3(audio_file_path).get_array_of_samples())
     overlap = 0
     windows = get_windows_from_array(audio_arr, Hz, overlap)
     windows = np.expand_dims(windows, axis=2)
-    predictions = model.predict(windows)
-    return round_predictions(predictions)
+    output = []
+    for window in windows:
+        window = np.reshape(window, (1, window.shape[0], window.shape[1]))
+        predictions = model.predict(window)
+        output.append(round_predictions(predictions))
+    return output
