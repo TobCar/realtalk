d792237aa6a6e5b4815020ba37d6c934d35a8bb1
shannonjanehogan <shannon.jane.hogan@gmail.com>
2019-09-14 16:04:39 -0400

    Fix get current time bug

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -7,14 +7,12 @@ var videoId = url.toString().substring(32, url.toString().length);
 /**
  *  MAKE REQUEST AND GET ARRAY
  */
+
 var res = [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0,0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1];
 
-var yt = document.querySelector('.video-stream');
-var timer;
-
-timer = setInterval(function() {  
-    yt = document.querySelector('.video-stream');
-    var time = Math.floor(yt.getCurrentTime());
+var timer = setInterval(function() {
+    var yt = document.querySelector('.video-stream');
+    var time = Math.floor(yt.currentTime);
     if (res[time]) {
         if (res[time] === 0) {
             chrome.runtime.sendMessage({ message: "Not a Voice" });
