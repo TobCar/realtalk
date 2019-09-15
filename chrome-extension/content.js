c8e04678f35768b5befa9b679cde455433293321
shannonjanehogan <shannon.jane.hogan@gmail.com>
2019-09-14 23:25:44 -0400

    Add event handling

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -10,15 +10,30 @@ var videoId = url.toString().substring(32, url.toString().length);
 
 var res = [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0,0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1];
 
-var timer = setInterval(function() {
-    var yt = document.querySelector('.video-stream');
-    var time = Math.floor(yt.currentTime);
-    if (res[time] !== undefined) {
-        if (res[time] === 0) {
-            chrome.runtime.sendMessage({ message: "fake" });
-        } else if (res[time] === 1) {
-            chrome.runtime.sendMessage({ message: "real" });
-        }
-    }
-}, 5000);
+var yt = document.querySelector('.video-stream');
+var timer;
+
+var tick = function() {
+  timer = setInterval(function() {
+      yt = document.querySelector('.video-stream');
+      var time = Math.floor(yt.currentTime);
+      if (res[time] !== undefined) {
+          if (res[time] === 0) {
+              chrome.runtime.sendMessage({ message: "fake" });
+          } else if (res[time] === 1) {
+              chrome.runtime.sendMessage({ message: "real" });
+          }
+      }
+  }, 5000);
+}
+
+yt.addEventListener("onStateChange", function(state) {
+  if (state === 1) {
+    tick();
+  } else {
+    clearInterval(timer);
+    chrome.runtime.sendMessage({ message: "default" });
+  }
+});
+
 // TODO double check that 5000 interval distance is correct
